dae38a0731d1702e32d6086784db97991e58d1c9
Emery <emery.jacobowitz@gmail.com>
2023-02-11 12:39:31 -0500

    refactored django

--- backend/snap/mysnappy/urls.py
+++ backend/snap/mysnappy/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.landing, name='landing'),
+    path('map/', views.map, name='map'),
+    path('shoppinglist/', views.shoppinglist, name='shoppinglist'),
 ]
