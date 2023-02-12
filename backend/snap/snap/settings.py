d44168380d36ef6afa35a9cbb508b6af87e1cebe
Emery <emery.jacobowitz@gmail.com>
2023-02-12 00:06:52 -0500

    removed secret keys

--- backend/snap/snap/settings.py
+++ backend/snap/snap/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bp&ch_@y&#woj-ujzd))w_i@b4hnk*otmey5xp!llvhfxw&nth"
+SECRET_KEY = os.environ['DJANGO_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
