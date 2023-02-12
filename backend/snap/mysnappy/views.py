7075c7c1aa1414f3989c22e2094140161556dea7
Emery <emery.jacobowitz@gmail.com>
2023-02-12 00:40:19 -0500

    decoupled

--- backend/snap/mysnappy/views.py
+++ backend/snap/mysnappy/views.py
@@ -3,17 +3,22 @@ from django.http import HttpResponse
 
 # TODO: implement spell-checking!
 
+# GET endpoints for the home page
 def landing(request):
     return render(request, "landing.html")
 
+# GET endpoints for grocery finder map.
 def map(request):
     return render(request, "map.html")
 
+# endpoints for Shopping List to recipe.
 def shoppinglist(request):
+    # GET: serve the page
     if request.method == "GET":
-        #print("GET")
         return render(request, "shoppinglist.html")
+
+    # POST: take in the shopping list. Query the recipe api for recipes.
+    # Send recipes back to the requester.
     elif request.method == "POST":
-        print("POST")
         return HttpResponse(request.POST['shoppingList'])
 
